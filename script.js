34cc72a2aa9e4dcb8bb6c9184126861c9a97135a
kapil212a <amangupta74086@gmail.com>
2025-09-18 15:27:22 +0530

    change

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-//Call Back Hell----->
-// function saveToDb(data , succes , failure){
-//     let internetSpeed = Math.floor(Math.random()*10)+1;
-//     if(internetSpeed > 4){
-//         succes();
-//     }else{
-//         failure();
-//     }
-// }
+Call Back Hell----->
+function saveToDb(data , succes , failure){
+    let internetSpeed = Math.floor(Math.random()*10)+1;
+    if(internetSpeed > 4){
+        succes();
+    }else{
+        failure();
+    }
+}
 
-// saveToDb(
-//     "apana college",
-//     () => {
-//     console.log("Data1 will saved");
-//     saveToDb("Aman gupta",
-//         () =>{
-//             console.log("Data2 will saved");
-//             saveToDb("New data",
-//                 ()=>{
-//                     console.log("Data3 will saved");
-//                 },()=>{
-//                     console.log("Data4 will not saved");
-//                 }
-//             )
-//         },
-//         ()=>{
-//             console.log("weak connection Data2 will not saved")
-//         }
-//     )
-//     },() =>{
-//     console.log("weak internet connection. Data will not saved");
-//     }
-// );
+saveToDb(
+    "apana college",
+    () => {
+    console.log("Data1 will saved");
+    saveToDb("Aman gupta",
+        () =>{
+            console.log("Data2 will saved");
+            saveToDb("New data",
+                ()=>{
+                    console.log("Data3 will saved");
+                },()=>{
+                    console.log("Data4 will not saved");
+                }
+            )
+        },
+        ()=>{
+            console.log("weak connection Data2 will not saved")
+        }
+    )
+    },() =>{
+    console.log("weak internet connection. Data will not saved");
+    }
+);
 
 // Changing The Color of Heading
 h1 = document.querySelector("h1");
