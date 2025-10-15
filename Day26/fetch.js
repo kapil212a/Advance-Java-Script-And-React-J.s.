0e6415b3826880eae8165dd7e485b78a3be6fbf8
kapil212a <amangupta74086@gmail.com>
2025-10-15 20:20:53 +0530

    change

---
+++ Day26/fetch.js
@@ -0,0 +1,18 @@
+let url = "http://catfact.ninja/fact";
+fetch(url)
+.then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+    console.log("data1 -", data.fact);
+    return fetch(url);
+})
+.then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+    console.log("data2 -",data.fact);
+})
+.catch((err)=>{
+    console.log("ERROR - ", err);
+});
