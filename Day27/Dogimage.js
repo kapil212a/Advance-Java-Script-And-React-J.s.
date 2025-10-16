cfc35a60671a3a4256da7489bbec4a75eb4ebaee
kapil212a <amangupta74086@gmail.com>
2025-10-16 20:18:28 +0530

    change

---
+++ Day27/Dogimage.js
@@ -0,0 +1,18 @@
+let url = "https://dog.ceo/api/breeds/image/random";
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click" , async()=>{
+    let link = await getImage();
+    let img = document.querySelector("#result");
+    img.setAttribute("src" , link);
+});
+
+async function getImage(){
+    try{
+        let res = await axios.get(url);
+        return res.data.message;
+    }catch(e){
+        console.log("image not found");
+    }
+}
