ccfea4988c856ce8f6d8f4844f5964378344e643
kapil212a <amangupta74086@gmail.com>
2025-10-31 19:54:00 +0530

    change

--- Day27/CatFacts.js
+++ Day27/CatFacts.js
@@ -8,11 +8,11 @@ btn.addEventListener("click", async() =>{
     let p = document.querySelector("#result");
     p.innerText = fact;
 });
-let url = "https://catfact.ninja/fact";
+let url = "https://catfact.ninja/fact";                    //API
 
 async function getFact(){
     try{
-        let res = await axios.get(url);
+        let res = await axios.get(url);                    //API calling
         return res.data.fact;
     }catch(err){
         console.log("fact not found");
