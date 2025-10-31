7dd62ff68e43c23f6b60e2e1ed89dece25e3f534
kapil212a <amangupta74086@gmail.com>
2025-10-31 19:52:51 +0530

    change

--- Day27/axios.js
+++ Day27/axios.js
@@ -2,7 +2,7 @@ let url = "https://catfact.ninja/fact";
 
 async function getFact(){
    try{
-        let res = await axios.get(url);
+        let res = await axios.get(url);          //use of axios
         console.log(res.data.fact);
    }catch(err){
     console.log("ERROR - " , err);
